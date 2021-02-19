eb79ad194038c15c034f74f19cb63b139964bf4a
Sergey Kudryashov <kudray@yandex.ru>
2021-02-19 16:51:03 +0300

    subrunner start

---
+++ subRunner.js
@@ -0,0 +1,27 @@
+//subRunner function
+
+function subRunner (puzzle) {
+
+  let column = 0;
+  let row = 0;
+
+  if (column > 8) {
+    row++;
+    column = 0;
+    if (row > 8 && column > 8) {
+        
+        return console.table(puzzle);
+    }
+  }
+  if puzzle[0]
+
+
+
+
+
+  
+
+
+
+}
+
