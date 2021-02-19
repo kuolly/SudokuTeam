f7b54a6878a3005af4eb278fc42d3e6d601b7d9d
Danya <danya.culakov@yandex.ru>
2021-02-19 16:42:05 +0300

    testttt

--- spec/sudoku.spec.js
+++ spec/sudoku.spec.js
@@ -1,2 +1,27 @@
-const sudoku = require('../sudoku.js')
+const sudoku = require('../sudoku.js'),
+const solveBox = require('../test.js')
 
+
+describe(boxArray, () => {
+  it('find sqer and remove value', () =>{
+    expect(boxArray()).toEqual()
+  })
+})
+
+describe(columnArray, () => {
+  it('define column', () =>{
+    expect(columnArray()).toEqual()
+  })
+})
+
+describe(rowArray, () => {
+  it('define row', () =>{
+    expect(rowArray()).toEqual()
+  })
+})
+
+describe(subrunner, () => {
+  it('subrunner', () =>{
+    expect(subrunner()).toEqual()
+  })
+})
